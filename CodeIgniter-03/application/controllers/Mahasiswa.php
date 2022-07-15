2878c9ddee62be3ae74d06e1a7c0fa07efb8c0e8
nessaaa <nnuranisahh@gmail.com>
2022-07-15 20:00:23 +0700

    Nahhh

--- CodeIgniter-03/application/controllers/Mahasiswa.php
+++ CodeIgniter-03/application/controllers/Mahasiswa.php
@@ -2,7 +2,7 @@
     class Mahasiswa extends CI_Controller{
         public function __construct(){
             parent::__construct();
-            $this->load->model("Mahasiswa_models");
+            $this->load->model("Mahasiswa_model");
         }
 
         public function index (){
