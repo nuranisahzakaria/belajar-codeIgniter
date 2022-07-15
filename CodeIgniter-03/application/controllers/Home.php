5816c038e906282d1603be0b0b7c0a48795936e5
nessaaa <nnuranisahh@gmail.com>
2022-07-15 20:32:20 +0700

    Soal no. 1 DONE

--- CodeIgniter-03/application/controllers/Home.php
+++ CodeIgniter-03/application/controllers/Home.php
@@ -2,8 +2,8 @@
     class Home extends CI_controller{
         public function index(){
             $this->load->view('templates/header');
-            $this->load->view('home/index');
-            $this->load->view('templates/footer');
+            $this->load->view('mahasiswa/index');
+            // $this->load->view('templates/footer');
         }
     }
 ?>
--- CodeIgniter-03/application/views/Mahasiswa/index.php
+++ CodeIgniter-03/application/views/Mahasiswa/index.php
@@ -8,18 +8,33 @@
     <title>Mahasiswa</title>
 </head>
 <body>
-    <div class="container">
+    <!-- <div class="container">
         <div class="row">
             <div class="col-md-6">
                 <h3>Daftar Mahasiswa CI Sederhana</h3>
 
                 <ul class="list-group">
-                    <?php foreach ($mahasiswa as $mhs) : ?>
-                    <li class="list-group-item"><?php echo $mhs ['nama']; ?></li>
-                    <?php endforeach ?>
+                    <?php //foreach ($mahasiswa as $mhs) : ?>
+                    <li class="list-group-item"><?php //echo $mhs ['nama']; ?></li>
+                    <?php //endforeach ?>
                 </ul>
             </div>
         </div>
-    </div>
+    </div> -->
+
+    <!-- TUGAS 1 -->
+    <div class="jumbotron">
+        <h1 class="display-4">Selamat Datang</h1>
+        <p class="lead">
+            Ini merupakan aplikasi latihan Pemograman Web Lanjutan menggunakan CodeIgniter dan Boostrap.
+        </p>
+        <hr class="my-4">
+        <p>
+            Nur Anisah, 200170015, Jurusan Teknik Informatika, Universitas Malikussaleh.
+        </p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="#" role="button">Lihat Data Lengkap</a>
+        </p>
+</div>
 </body>
 </html>
