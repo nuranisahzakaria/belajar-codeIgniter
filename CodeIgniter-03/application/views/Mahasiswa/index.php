1213de136a07a5aa6942c3aae6b7b65507aa364e
nessaaa <nnuranisahh@gmail.com>
2022-07-15 20:43:39 +0700

    Cara buat dan nampilin beberapa file di CI gimana.. Harusnya nanti di headernya ada link yang menuju ke file lain.

--- CodeIgniter-03/application/views/Mahasiswa/index.php
+++ CodeIgniter-03/application/views/Mahasiswa/index.php
@@ -22,7 +22,7 @@
         </div>
     </div> -->
 
-    <!-- TUGAS 1 -->
+    <!-- SOAL 1 -->
     <div class="jumbotron">
         <h1 class="display-4">Selamat Datang</h1>
         <p class="lead">
@@ -35,6 +35,41 @@
         <p class="lead">
             <a class="btn btn-primary btn-lg" href="#" role="button">Lihat Data Lengkap</a>
         </p>
-</div>
+    </div>
+
+    <!-- SOAL 2 -->
+    <!-- <table class="table table-dark">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table> -->
+
+    <!-- SOAL 3 -->
+
 </body>
 </html>
