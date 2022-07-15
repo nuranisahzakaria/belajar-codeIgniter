53e9e05aec4b269297bcc2aa523bb667932d91d9
nessaaa <nnuranisahh@gmail.com>
2022-07-15 18:55:06 +0700

    Ini udah ngga error kayak tadi lagi, tapi errornya ganti. Lupaa, aku belum buat file header.php sama footer.php, ngga ngerti buatnya wkwkwk. Magrib, nanti lanjut lagi.

--- CodeIgniter-03/application/models/Mahasiswa_model.php
+++ CodeIgniter-03/application/models/Mahasiswa_model.php
@@ -1,5 +1,5 @@
 <?php
-class Mahasiswa_models extends CI_Model{
+class Mahasiswa_model extends CI_Model{
     public function getAllMahasiswa(){
         $query=$this->this->db->get('mahasiswa');
         return $query->result_array();
--- CodeIgniter-03/application/views/Mahasiswa/index.php
+++ CodeIgniter-03/application/views/Mahasiswa/index.php
@@ -14,8 +14,8 @@
                 <h3>Daftar Mahasiswa CI Sederhana</h3>
 
                 <ul class="list-group">
-                    <?php foreach ($perpustakaan as $mhs) : ?>
-                    <li class="list-group-item"><?php echo $mhs ['judul_buku']; ?></li>
+                    <?php foreach ($mahasiswa as $mhs) : ?>
+                    <li class="list-group-item"><?php echo $mhs ['nama']; ?></li>
                     <?php endforeach ?>
                 </ul>
             </div>
