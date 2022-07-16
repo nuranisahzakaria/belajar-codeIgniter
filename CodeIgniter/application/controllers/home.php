5d787ebebaedcacdc1e717eefc8c8962d21dc9a2
nessaaa <nnuranisahh@gmail.com>
2022-07-16 21:04:50 +0700

    Ngga mau ambil resiko, seenggaknya kalo mau kumpul udah ada filenya. Takutnya nanti kalo langsung diubah-ubah malah error lagi.

--- CodeIgniter/application/controllers/home.php
+++ CodeIgniter/application/controllers/home.php
@@ -4,10 +4,8 @@
             $this->load->model('mahasiswa_model');
             $data['mahasiswa'] = $this->mahasiswa_model->get_data();
 
-            //$this->load->view('view_mahasiswa', $data);
-
             $this->load->view ('templates/header');
-            $this->load->view ('mahasiswa/tentang_kami', $data);
+            $this->load->view ('mahasiswa/index', $data);
             $this->load->view ('templates/footer');
         }
     }
---
+++ CodeIgniter/application/views/mahasiswa/beranda.php
@@ -0,0 +1,13 @@
+<div class="jumbotron">
+    <h1 class="display-4">Selamat Datang</h1>
+    <p class="lead">
+        Ini merupakan aplikasi latihan Pemograman Web Lanjutan menggunakan CodeIgniter dan Boostrap.
+    </p>
+    <hr class="my-4">
+    <p>
+        Nur Anisah, 200170015, Jurusan Teknik Informatika, Universitas Malikussaleh.
+    </p>
+    <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Lihat Data Lengkap</a>
+    </p>
+</div>
